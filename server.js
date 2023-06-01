2720ff10e1ab462957bfc9455bbe724f8b57e626
sangam verma <sangamverma955911@gmail.com>
2023-06-01 15:08:37 +0530

    1.0.5

--- server.js
+++ server.js
@@ -15,9 +15,34 @@ app.use(express.json());
 app.use("/api/users", userroute);
 app.use("/api/notes", notesroute);
 //-------------
-const cors = require("cors");
+// const cors = require("cors");
 
-app.use(cors({ origin: "*" }));
+// app.use(cors({ origin: "*" }));
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 //---------------
 
 app.get("/", (req, res) => {
